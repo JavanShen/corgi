76aa3a36ee70d33e42e140ff41e287cc55a44519
apine <1985486089@qq.com>
2022-10-18 13:36:11 +0800

    :construction: 音频播放composables

---
+++ packages/composables/index.ts
@@ -0,0 +1,1 @@
+export { default as usePlayAudio } from './player/usePlayAudio'
---
+++ packages/composables/player/usePlayAudio.ts
@@ -0,0 +1,50 @@
+import { Player } from '@corgi/utils'
+import type { Source } from '@corgi/types'
+import debounce from 'lodash/debounce'
+
+export default function usePlayAudio(source: Source) {
+    const currentTime = ref(0)
+    const totalTime = ref(0)
+    const totalTimeText = ref('00:00')
+    const isManualUpdating = ref(false)
+
+    const player = new Player(source)
+
+    player.loadedData(() => {
+        totalTime.value = Math.floor(player.duration)
+        totalTimeText.value = player.totalTimeText
+    })
+
+    player.timeUpdate(() => {
+        if (!isManualUpdating.value) {
+            currentTime.value = Math.floor(player.currentTime)
+        }
+    })
+
+    const play = () => {
+        player.play()
+    }
+
+    const pause = () => {
+        player.pause()
+    }
+
+    const updateAudioTime = debounce((val: number) => {
+        player.currentTime = val
+    }, 500)
+    const updateTime = (val: number) => {
+        isManualUpdating.value = true
+        currentTime.value = val
+        updateAudioTime(val)
+        isManualUpdating.value = false
+    }
+
+    return {
+        currentTime,
+        totalTime,
+        totalTimeText,
+        play,
+        pause,
+        updateTime
+    }
+}
