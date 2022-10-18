5a373cbfff26e455801958257bdfa8ffb3aec248
apine <1985486089@qq.com>
2022-10-18 23:22:21 +0800

    :wrench: 增加jsx支持, 更新unplugin配置

--- preview/vue/vite.config.ts
+++ preview/vue/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
@@ -8,14 +9,14 @@ import AutoImport from 'unplugin-auto-import/vite'
 export default defineConfig({
     plugins: [
         vue(),
+        vueJsx(),
         Components({
-            dirs: ['../../demo'],
             dts: '../../components.d.ts',
             resolvers: [NaiveUiResolver()]
         }),
         AutoImport({
             imports: ['vue'],
-            dts: '../../auto-imports.d.ts',
+            dts: '../../auto-imports-vue.d.ts',
             vueTemplate: true
         })
     ]
