c091486b90a2e5ddaded84bd3fd174232376cfe2
apine <1985486089@qq.com>
2022-10-18 13:37:14 +0800

    :construction: 音频播放器组件

---
+++ packages/vue/AudioPlayer/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <n-card hoverable>
+        <n-slider
+            :value="currentTime"
+            :max="totalTime"
+            :tooltip="false"
+            @update-value="updateTime"
+        />
+        <span>{{ totalTimeText }}</span>
+        <n-button @click="play">开始</n-button>
+        <n-button @click="pause">暂停</n-button>
+    </n-card>
+</template>
+
+<script setup lang="ts">
+import { usePlayAudio } from '@corgi/composables'
+import type { Source } from '@corgi/types'
+
+interface Props {
+    source: Source
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    source: ''
+})
+
+const { currentTime, totalTime, totalTimeText, play, pause, updateTime } =
+    usePlayAudio(props.source)
+</script>
