88929b42594af1e8304009f0cd1662904f628437
apine <1985486089@qq.com>
2022-10-22 14:43:07 +0800

    :alien: 解决类型冲突

--- packages/utils/__test__/file.spec.ts
+++ packages/utils/__test__/file.spec.ts
@@ -1,8 +1,9 @@
+import { expect, it } from 'vitest'
 import { arrayBufferToBase64 } from '../file'
 
 // @vitest-environment happy-dom
 
-test('ArrayBuffer对象转base64', () => {
+it('ArrayBuffer对象转base64', () => {
     const buffer = new ArrayBuffer(1)
     expect(arrayBufferToBase64(buffer)).toBeTypeOf('string')
 })
--- packages/utils/__test__/time.spec.ts
+++ packages/utils/__test__/time.spec.ts
@@ -1,6 +1,7 @@
+import { it, expect } from 'vitest'
 import { secondsToMinutes } from '../time'
 
-test('秒转分钟', () => {
+it('秒转分钟', () => {
     expect(secondsToMinutes(25)).toBe('00:25')
     expect(secondsToMinutes(90)).toBe('01:30')
 })
