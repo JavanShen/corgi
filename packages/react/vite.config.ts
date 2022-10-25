649c09a2c4d227064e69e30be23bc73a2737ffa3
apine <1985486089@qq.com>
2022-10-25 16:09:00 +0800

    :wrench: 配置autoimport, 取消工作区外文件访问的限制(cypress)

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()]
+    plugins: [
+        react(),
+        AutoImport({
+            imports: ['react']
+        })
+    ],
+    server: {
+        fs: {
+            strict: false
+        }
+    }
 })
