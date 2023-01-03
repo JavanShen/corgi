16986e4fa0eb4d6eb233351ca1902b339fed1a41
apine <1985486089@qq.com>
2023-01-03 18:11:05 +0800

    :fire: 移除 demo 和 preview，用 docs 替代

--- demo/AudioPlayer/source.ts
+++
@@ -1,6 +0,0 @@
-import path from './music/demo.mp3'
-
-const link =
-    'https://howlerjs.com/assets/howler.js/examples/player/audio/rave_digger.webm'
-
-export { path, link }
--- preview/vue/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <VAudioPlayerLink />
-    <VAudioPlayerPath />
-</template>
-<script setup lang="ts">
-import { VAudioPlayerLink, VAudioPlayerPath } from '@corgi/demo/vue'
-</script>
