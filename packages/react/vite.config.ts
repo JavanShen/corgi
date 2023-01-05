b797775c0aee6ce15016c34fe0e1a600a02638a4
apine <1985486089@qq.com>
2023-01-05 20:35:18 +0800

    :sparkles: 使用 svgr 来导入 svg

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
+import svgr from 'vite-plugin-svgr'
 import { resolve } from 'path'
 
 // https://vitejs.dev/config/
@@ -11,7 +12,8 @@ export default defineConfig({
             tsConfigFilePath: resolve(__dirname, './tsconfig.json'),
             outputDir: [resolve(__dirname, 'es'), resolve(__dirname, 'lib')]
         }),
-        react()
+        react(),
+        svgr()
     ],
     server: {
         fs: {
