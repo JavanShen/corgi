8fd1a17e7d923a7fbe42fca2dfc5b7e4d4045d07
apine <1985486089@qq.com>
2023-01-08 21:20:14 +0800

    ✅  @corgii/react AudioPlayer 更细致划分测试类型

--- packages/react/cypress/component/AudioPlayer.cy.tsx
+++ packages/react/cypress/component/AudioPlayer.cy.tsx
@@ -25,123 +25,151 @@ const {
 const AudioLink = () => <AudioPlayer source={link} />
 
 describe('<AudioPlayer>', () => {
-    it('初始化成功', () => {
-        readMP3File().then(file => {
-            cy.mount(<AudioPlayer source={file} />)
+    describe('初始化', () => {
+        it('显示时长', () => {
+            readMP3File().then(file => {
+                cy.mount(<AudioPlayer source={file} />)
 
-            cy.get(playSelector).should('be.visible')
-            cy.get(currentTimeTextSelector).should('have.text', '00:00')
-            cy.get(totalTimeTextSelector).should('have.text', '00:05')
+                cy.get(playSelector).should('be.visible')
+                cy.get(currentTimeTextSelector).should('have.text', '00:00')
+                cy.get(totalTimeTextSelector).should('have.text', '00:05')
+            })
         })
-    })
 
-    it('使用自定义的信息', () => {
-        cy.mount(
-            <AudioPlayer
-                source={link}
-                title="标题"
-                artist="创作者"
-                cover={picLink}
-            />
-        )
+        it('默认标题和创作者', () => {
+            cy.mount(<AudioLink />)
 
-        cy.get(titleSelector).should('have.text', '标题')
-        cy.get(artistSelector).should('have.text', '创作者')
-        cy.get(coverSelector).find('img').should('have.attr', 'src', picLink)
-    })
+            cy.get(titleSelector).should('have.text', '未知')
+            cy.get(artistSelector).should('have.text', '未知')
+        })
 
-    it('使用音量控件', () => {
-        readMP3File().then(file => {
-            cy.mount(<AudioPlayer source={file} showVolumeControl />)
+        it('显示自定义的信息', () => {
+            cy.mount(
+                <AudioPlayer
+                    source={link}
+                    title="标题"
+                    artist="创作者"
+                    cover={picLink}
+                />
+            )
 
-            cy.get(volumeButtonSelector).as('volumeBtn')
+            cy.get(titleSelector).should('have.text', '标题')
+            cy.get(artistSelector).should('have.text', '创作者')
+            cy.get(coverSelector)
+                .find('img')
+                .should('have.attr', 'src', picLink)
+        })
 
-            cy.get(volumeIconSelector)
-                .should('be.visible')
-                .get('@volumeBtn')
-                .click()
-                .find(muteIconSelector)
-                .should('be.visible')
-                .get('@volumeBtn')
-                .click()
-                .find(volumeIconSelector)
-                .should('be.visible')
+        it('隐藏封面', () => {
+            readMP3File().then(file => {
+                cy.mount(<AudioPlayer source={file} cover={false} />)
 
-            cy.get('@volumeBtn')
-                .trigger('mouseover')
-                .get('.volume-control')
-                .should('be.visible')
-                .find('.ant-slider-handle')
-                .as('slider')
+                cy.get(coverSelector).should('not.exist')
+            })
+        })
 
-            move('@slider', 0, 50).then(() => {
-                cy.get(playSelector).click()
-                cy.wait(3000)
+        it('显示ID3解析内容', () => {
+            readMP3File().then(file => {
+                cy.mount(<AudioPlayer source={file} />)
+
+                cy.get(titleSelector).should(
+                    'have.text',
+                    'Super Mario Bros. / プレイヤーダウン'
+                )
+                cy.get(artistSelector).should('have.text', '近藤浩治')
+                cy.get(coverSelector).find('img').should('have.attr', 'src')
+            })
+        })
+
+        it('加载完成后触发回调', () => {
+            readMP3File().then(file => {
+                const onLoaded = cy.spy().as('onLoaded')
+
+                cy.mount(<AudioPlayer source={file} loaded={onLoaded} />)
+                cy.get('@onLoaded').should('have.been.called')
             })
         })
     })
 
-    it('隐藏封面', () => {
-        readMP3File().then(file => {
-            cy.mount(<AudioPlayer source={file} cover={false} />)
+    describe('操作', () => {
+        it('播放和暂停', () => {
+            readMP3File().then(file => {
+                cy.mount(<AudioPlayer source={file} />)
 
-            cy.get(coverSelector).should('not.exist')
+                cy.get(playSelector).click()
+                cy.get(pauseSelector).should('be.visible')
+
+                cy.wait(2000)
+
+                cy.get(currentTimeTextSelector).should('have.text', '00:03')
+                cy.get(pauseSelector).click()
+                cy.get(playSelector).should('be.visible')
+            })
         })
-    })
 
-    it('加载完成后触发回调', () => {
-        readMP3File().then(file => {
-            const onLoaded = cy.spy().as('onLoaded')
+        describe('音量控件', () => {
+            it('调整音量', () => {
+                readMP3File().then(file => {
+                    cy.mount(<AudioPlayer source={file} showVolumeControl />)
 
-            cy.mount(<AudioPlayer source={file} loaded={onLoaded} />)
-            cy.get('@onLoaded').should('have.been.called')
+                    cy.get(volumeButtonSelector).as('volumeBtn')
+
+                    cy.get(volumeIconSelector)
+                        .should('be.visible')
+                        .get('@volumeBtn')
+                        .click()
+                        .find(muteIconSelector)
+                        .should('be.visible')
+                        .get('@volumeBtn')
+                        .click()
+                        .find(volumeIconSelector)
+                        .should('be.visible')
+
+                    cy.get('@volumeBtn')
+                        .trigger('mouseover')
+                        .get('.volume-control')
+                        .should('be.visible')
+                        .find('.ant-slider-handle')
+                        .as('slider')
+
+                    move('@slider', 0, 50).then(() => {
+                        cy.get(playSelector).click()
+                        cy.wait(3000)
+                    })
+                })
+            })
         })
-    })
 
-    it('播放和暂停', () => {
-        readMP3File().then(file => {
-            cy.mount(<AudioPlayer source={file} />)
+        describe('进度条', () => {
+            const dragProcessBarTest = () => {
+                cy.get('.progress-bar')
+                    .find('.ant-slider-handle')
+                    .as('sliderBtn')
 
-            cy.get(playSelector).click()
-            cy.get(pauseSelector).should('be.visible')
+                return move('@sliderBtn', 120, 0)
+                    .get(currentTimeTextSelector)
+                    .should('have.text', '00:03')
+            }
 
-            cy.wait(2000)
+            it('暂停时拖动进度条', () => {
+                readMP3File().then(file => {
+                    cy.mount(<AudioPlayer source={file} />).wait(1500)
 
-            cy.get(currentTimeTextSelector).should('have.text', '00:03')
-            cy.get(pauseSelector).click()
-            cy.get(playSelector).should('be.visible')
+                    dragProcessBarTest()
+                })
+            })
+
+            it('播放时拖动进度条', () => {
+                readMP3File().then(file => {
+                    cy.mount(<AudioPlayer source={file} />).wait(1500)
+
+                    cy.get(playSelector).click()
+
+                    dragProcessBarTest()
+
+                    cy.get(pauseSelector).click()
+                })
+            })
         })
-    })
-
-    it('拖动进度条', () => {
-        readMP3File().then(file => {
-            cy.mount(<AudioPlayer source={file} />).wait(1500)
-
-            cy.get('.progress-bar').find('.ant-slider-handle').as('sliderBtn')
-
-            move('@sliderBtn', 120, 0)
-                .get(currentTimeTextSelector)
-                .should('have.text', '00:03')
-        })
-    })
-
-    it('ID3标签解析成功', () => {
-        readMP3File().then(file => {
-            cy.mount(<AudioPlayer source={file} />)
-
-            cy.get(titleSelector).should(
-                'have.text',
-                'Super Mario Bros. / プレイヤーダウン'
-            )
-            cy.get(artistSelector).should('have.text', '近藤浩治')
-            cy.get(coverSelector).find('img').should('have.attr', 'src')
-        })
-    })
-
-    it('ID3标签解析失败', () => {
-        cy.mount(<AudioLink />)
-
-        cy.get(titleSelector).should('have.text', '未知')
-        cy.get(artistSelector).should('have.text', '未知')
     })
 })
