c070c08a748de1c8352cbaff18e498f74ea85c1d
apine <1985486089@qq.com>
2023-01-08 20:36:19 +0800

    🩹  改用 getBoundingClientRect 获取元素位置，事件触发之间添加停留时间

--- packages/react/cypress/utils/action.ts
+++ packages/react/cypress/utils/action.ts
@@ -1,15 +1,15 @@
 const move = (target: string, offsetX: number, offsetY: number) => {
     return cy.get(target).then(el => {
-        const offset = el.offset()
-        const x = offset?.left
-        const y = offset?.top
+        const { left: x, top: y } = el[0].getBoundingClientRect()
 
         cy.get(target)
             .trigger('mousedown', { button: 0 })
+            .wait(500)
             .trigger('mousemove', {
                 pageX: x && x + offsetX,
                 pageY: y && y + offsetY
             })
+            .wait(500)
             .trigger('mouseup', { force: true })
     })
 }
