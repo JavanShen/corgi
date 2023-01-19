f7fd4b54a49e85f00661604712a3c71573b44c17
apine <1985486089@qq.com>
2023-01-19 10:10:07 +0800

    👽️  使用 - 分隔路径

--- demo/react/examples/Overview.tsx
+++ demo/react/examples/Overview.tsx
@@ -2,13 +2,31 @@ import { Card, List, Image } from 'antd'
 import { Link } from 'react-router-dom'
 import componentsList from '../componentsList'
 
+const hyphen = (str: string) => {
+    const [first, ...rest] = str.split('')
+    return [
+        first.toLowerCase(),
+        ...rest.reduce((pre, cur) => {
+            const charCode = cur.charCodeAt(0)
+
+            if (charCode >= 65 && charCode <= 90) {
+                pre.push('-', cur.toLowerCase())
+            } else {
+                pre.push(cur)
+            }
+
+            return pre
+        }, [] as string[])
+    ].join('')
+}
+
 const Overview = () => (
     <List
         grid={{ gutter: 16, column: 3 }}
         dataSource={componentsList}
         renderItem={({ name, label }) => (
             <List.Item>
-                <Link to={`/components/${name}`}>
+                <Link to={`/components/${hyphen(name)}`}>
                     <Card title={`${name} ${label}`} hoverable>
                         <Image src={`../assets/${name}.png`} preview={false} />
                     </Card>
