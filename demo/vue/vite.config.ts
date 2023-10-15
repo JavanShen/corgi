63e343df8f1118d5e87a604c370d5d6849a188c8
apine <1985486089@qq.com>
2023-10-15 22:52:55 +0800

    ➖  移除 unplugin-vue-components

--- demo/vue/vite.config.ts
+++ demo/vue/vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import svgLoader from 'vite-svg-loader'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueJsx(),
-        Components({
-            dts: resolve(__dirname, '../../packages/vue/components.d.ts'),
-            resolvers: [AntDesignVueResolver()]
-        }),
-        svgLoader()
-    ]
+    plugins: [vue(), vueJsx(), svgLoader()]
 })
